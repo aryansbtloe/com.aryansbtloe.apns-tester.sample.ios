761df74f8c00340c32bfb22a375dd6627f712a88
Alok Singh <alok.singh2@oyorooms.com>
2020-06-03 03:03:13 +0530

    1.0#4

--- Application/HomeViewController.swift
+++ Application/HomeViewController.swift
@@ -15,6 +15,7 @@ class HomeViewController:BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchAndCopyDeviceToken()
     }
     
     @IBAction func fetchAndCopyDeviceToken(){
