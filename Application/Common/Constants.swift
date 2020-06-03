d12971665a5fdf54f88b709c02e7961c18631d67
Alok Singh <alok.singh2@oyorooms.com>
2020-06-04 01:42:49 +0530

    #newarchitecture

---
+++ Application/Common/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Application
+//
+//  Created by Alok Singh on 6/3/20.
+//  Copyright © 2020 Alok Singh. All rights reserved.
+//
+
+import Foundation
+
+let kAdUnitId = "ca-app-pub-7178447751314817/2069445475"
---
+++ Application/Common/Utility/Utility + Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Utility.swift
+//  
+//
+//  Created by Alok Singh on 4/18/20.
+//
+
+import Foundation
+import UIKit
+
+extension Utility {
+
+}
+
